76c0557d825f3defbd632c26e2d43a26ac605fe4
Andreas Weis <github@andreas-weis.net>
2025-09-16 18:50:31 +0200

    [#1055] Add gdb pretty printing for StaticString

--- iceoryx2-bb/cxx/iceoryx2-bb-containers-cxx-pretty-printer.py
+++ iceoryx2-bb/cxx/iceoryx2-bb-containers-cxx-pretty-printer.py
@@ -32,10 +32,28 @@ class OptionalPrinter:
             nested_type = self.val['m_value']['m_u_value'].type.strip_typedefs()
             return f"{{value = {self.val['m_value']['m_u_value'].cast(nested_type)}}}"
 
+class StaticStringPrinter:
+    "Print an iox2::container::StaticString"
+
+    def __init__(self, val, *, static_string_capacity):
+        self.val = val
+        self.static_string_capacity = static_string_capacity
+
+    def to_string(self):
+        if int(self.val['m_size']) == 0:
+            return "\"\""
+        else:
+            string_type = gdb.lookup_type('char').const().pointer()
+            string_contents = f"{self.val['m_string'].cast(string_type)}"
+            return string_contents[string_contents.find('\"'):]
+
 def iox2_bb_containers_cxx(val):
     iox2_bb_containers_cxx.rx_optional = re.compile("^(?:const )?iox2::container::Optional<(.*)>$")
+    iox2_bb_containers_cxx.rx_static_string = re.compile("^(?:const )?iox2::container::StaticString<(.*)>$")
     if (match := iox2_bb_containers_cxx.rx_optional.match(str(val.type))) is not None:
         return OptionalPrinter(val, contained_type=match[1])
+    elif (match := iox2_bb_containers_cxx.rx_static_string.match(str(val.type))) is not None:
+        return StaticStringPrinter(val, static_string_capacity=match[1])
     else:
         return None
 
