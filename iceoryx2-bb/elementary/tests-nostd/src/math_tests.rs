5d741e741bb9ab817876c80561126956fe973b08
Jeff Ithier <jeff@ithier.dev>
2026-02-02 14:21:15 +0100

    [#1300] Add no_std test suite for iceoryx2-bb-elementary

--- iceoryx2-bb/elementary/tests-nostd/src/math_tests.rs
+++ iceoryx2-bb/elementary/tests-nostd/src/math_tests.rs
@@ -11,23 +11,24 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 use iceoryx2_bb_elementary_tests_common::math_tests;
+use iceoryx2_bb_testing_nostd_macros::inventory_test;
 
-#[test]
+#[inventory_test]
 pub fn math_align_returns_input_when_already_aligned() {
     math_tests::math_align_returns_input_when_already_aligned();
 }
 
-#[test]
+#[inventory_test]
 pub fn math_align_returns_input_to_next_greater_value() {
     math_tests::math_align_returns_input_to_next_greater_value();
 }
 
-#[test]
+#[inventory_test]
 pub fn math_dec_to_64() {
     math_tests::math_dec_to_64();
 }
 
-#[test]
+#[inventory_test]
 pub fn const_max_works() {
     math_tests::const_max_works();
 }
