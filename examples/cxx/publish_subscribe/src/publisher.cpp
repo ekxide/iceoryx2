450346e841fe09c5c67401636aeed380b6ef6d20
Mathias Kraus <elboberido@m-hias.de>
2026-05-15 23:12:24 +0200

    Print node state in cleaner

--- examples/cxx/publish_subscribe/src/publisher.cpp
+++ examples/cxx/publish_subscribe/src/publisher.cpp
@@ -14,14 +14,62 @@
 #include "transmission_data.hpp"
 
 #include <iostream>
-#include <utility>
+// #include <utility>
 
-constexpr iox2::bb::Duration CYCLE_TIME = iox2::bb::Duration::from_secs(1);
+// constexpr iox2::bb::Duration CYCLE_TIME = iox2::bb::Duration::from_secs(1);
 
 auto main() -> int {
     using namespace iox2;
     set_log_level_from_env_or(LogLevel::Info);
-    auto node = NodeBuilder().create<ServiceType::Ipc>().value();
+
+    auto config = Config::global_config().to_owned();
+    config.global().node().set_cleanup_dead_nodes_on_creation(true);
+    config.global().node().set_cleanup_dead_nodes_on_destruction(true);
+    config.global().service().set_cleanup_dead_nodes_on_open(true);
+
+    auto list_nodes = [&] {
+        Node<ServiceType::Ipc>::list(config.view(), [](auto node_state) {
+            node_state.alive([](const AliveNodeView<ServiceType::Ipc>& view) {
+                if (view.details().has_value()) {
+                    std::cout << "  alive: " << view.details()->executable().as_string() << std::endl;
+                } else {
+                    std::cout << "  alive: " << view.id() << std::endl;
+                }
+            });
+            node_state.dead([](const DeadNodeView<ServiceType::Ipc>& view) {
+                if (view.details().has_value()) {
+                    std::cout << "  dead: " << view.details()->executable().as_string() << std::endl;
+                } else {
+                    std::cout << "  dead: " << view.id() << std::endl;
+                }
+            });
+            node_state.inaccessible(
+                [](const UniqueNodeId& view) { std::cout << "  inaccessible: " << view << std::endl; });
+            node_state.undefined([](const UniqueNodeId& view) { std::cout << "  undefined: " << view << std::endl; });
+
+
+            return CallbackProgression::Continue;
+        });
+    };
+
+    {
+        std::cout << "before cleanup via node creation" << std::endl;
+        list_nodes();
+
+        auto node = NodeBuilder().config(config).create<ServiceType::Ipc>().value();
+
+        std::cout << "before cleanup via service open" << std::endl;
+        list_nodes();
+
+        node.service_builder(ServiceName::create("My/Funk/ServiceName").value())
+            .request_response<uint64_t, TransmissionData>()
+            .max_servers(2)
+            .max_clients(1)
+            .open_or_create();
+    }
+
+    std::cout << "after cleanup" << std::endl;
+    list_nodes();
 
     // auto service = node.service_builder(ServiceName::create("My/Funk/ServiceName").value())
     //                    .publish_subscribe<TransmissionData>()
