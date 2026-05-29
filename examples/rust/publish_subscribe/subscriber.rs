dafcf91457e06fd8b0d7bd1e6010d6dced2b906a
dkroenke <dkroenke@posteo.com>
2026-05-29 10:49:22 +0200

    Reduce cycle time in pubsub example

--- examples/rust/publish_subscribe/subscriber.rs
+++ examples/rust/publish_subscribe/subscriber.rs
@@ -19,7 +19,7 @@ use examples_common::TransmissionData;
 use iceoryx2::prelude::*;
 use iceoryx2_log::cout;
 
-const CYCLE_TIME: Duration = Duration::from_secs(1);
+const CYCLE_TIME: Duration = Duration::from_millis(1);
 
 fn main() -> Result<(), Box<dyn core::error::Error>> {
     set_log_level_from_env_or(LogLevel::Info);
