4a1e9b66300c10661c4da465c9b23fbe8d8153c9
Jeff Ithier <jeff@ithier.dev>
2026-09-15 15:04:44 +0200

    [#1977] Add the link adapter crate

--- iceoryx2-link/backend/src/service_description/mod.rs
+++ iceoryx2-link/backend/src/service_description/mod.rs
@@ -127,10 +127,10 @@ impl core::fmt::Display for UnsupportedPattern {
 
 impl core::error::Error for UnsupportedPattern {}
 
-impl TryFrom<&StaticConfig> for ServiceDescription {
+impl<S: Service> TryFrom<&StaticConfig<S>> for ServiceDescription {
     type Error = UnsupportedPattern;
 
-    fn try_from(static_config: &StaticConfig) -> Result<Self, Self::Error> {
+    fn try_from(static_config: &StaticConfig<S>) -> Result<Self, Self::Error> {
         let (pattern, types) = match static_config.messaging_pattern() {
             StaticPattern::PublishSubscribe(config) => {
                 let types = config.message_type_details();
@@ -192,7 +192,7 @@ mod tests {
         name: &ServiceName,
         config: &Config,
         pattern: Pattern,
-    ) -> StaticConfig {
+    ) -> StaticConfig<local::Service> {
         local::Service::details(name, config, pattern)
             .expect("details are readable")
             .expect("service exists")
